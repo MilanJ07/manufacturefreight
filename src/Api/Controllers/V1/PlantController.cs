86dbda7ba368cea5d0baf1d28cc08360270709b3
Aman Sinha <aman.s@diverseinfotech.com>
2024-02-29 01:04:17 +0530

    Fixed

--- src/Api/Controllers/V1/PlantController.cs
+++ src/Api/Controllers/V1/PlantController.cs
@@ -8,7 +8,7 @@ using System;
 namespace Api.Controllers.V1
 {
     [ApiVersion("1.0")]
-    [Route("api/v{version:apiVersion}/[controller]/plant")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class PlantController : CssControllerBase
     {
